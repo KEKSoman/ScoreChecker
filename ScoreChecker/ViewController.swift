070d00f1cb3df0bbc8af79aa47fc789c21c92458
KEKSman <bassman7009@gmail.com>
2022-06-08 21:06:03 +0700

    Initial Commit

---
+++ ScoreChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreChecker
+//
+//  Created by Евгений колесников on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
